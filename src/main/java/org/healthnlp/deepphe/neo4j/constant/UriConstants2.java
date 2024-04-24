b9095792fbac838c9e6cb2640a9c1bc0c30a9c3f
Sean Finan <sean.finan@childrens.harvard.edu>
2024-04-24 15:29:16 -0400

    Simple method to fetch reportability

--- src/main/java/org/healthnlp/deepphe/neo4j/constant/UriConstants2.java
+++ src/main/java/org/healthnlp/deepphe/neo4j/constant/UriConstants2.java
@@ -1,10 +1,12 @@
 package org.healthnlp.deepphe.neo4j.constant;
 
+import org.apache.log4j.Logger;
 import org.healthnlp.deepphe.neo4j.util.SearchUtil;
-import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.*;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Objects;
 
 /**
  * @author SPF , chip-nlp
@@ -30,14 +32,23 @@ final public class UriConstants2 {
    }
 
    static private final Collection<String> RIGHT_LUNG_LOBES = new HashSet<>();
-   static public Collection<String> getRightLungLobes(final GraphDatabaseService graphDb ) {
+
+   static public Collection<String> getRightLungLobes( final GraphDatabaseService graphDb ) {
       initializeUris( graphDb );
       return RIGHT_LUNG_LOBES;
    }
 
 
+   static private final Collection<String> REPORTABLE_URIS = new HashSet<>();
+
+   static public Collection<String> getReportableUris( final GraphDatabaseService graphDb ) {
+      initializeUris( graphDb );
+      return REPORTABLE_URIS;
+   }
+
 
    static private final Object URI_LOCK = new Object();
+
    static private void initializeUris( final GraphDatabaseService graphDb ) {
       synchronized ( URI_LOCK ) {
          if ( !QUADRANT_URIS.isEmpty() ) {
@@ -54,6 +65,19 @@ final public class UriConstants2 {
          final Collection<String> lungLobes = SearchUtil.getBranchUris( graphDb, "LungLobe" );
          lungLobes.stream().filter( u -> u.contains( "Left" ) ).forEach( LEFT_LUNG_LOBES::add );
          lungLobes.stream().filter( u -> u.contains( "Right" ) ).forEach( RIGHT_LUNG_LOBES::add );
+
+         try ( Transaction tx = graphDb.beginTx() ) {
+            final ResourceIterator<Node> nodes = graphDb.findNodes( Label.label( "Reportable" ) );
+            nodes.stream()
+                 .map( n -> n.getProperty( Neo4jConstants.NAME_KEY ) )
+                 .filter( Objects::nonNull )
+                 .map( Object::toString )
+                 .forEach( REPORTABLE_URIS::add );
+            nodes.close();
+            tx.success();
+         } catch ( MultipleFoundException mfE ) {
+            Logger.getLogger( "UriConstants2" ).error( mfE.getMessage(), mfE );
+         }
       }
    }
 
